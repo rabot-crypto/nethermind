b44ea32fe76b73cdc9b4cdff71942c547de1402e
teneko <giteroneko@gmail.com>
2022-05-12 18:56:39 +0100

    Updated GitHub workflow

--- rabot/scripts/publish-package.sh
+++ rabot/scripts/publish-package.sh
@@ -13,7 +13,4 @@ dotnet pack -c $CONFIGURATION "src/RabotCrypto.Nethermind.Db.Rocks"
 dotnet nuget push "src/RabotCrypto.Nethermind.Api/bin/Release/RabotCrypto.Nethermind.Api.$VERSION.nupkg" --source "$NUGET_SOURCE"
 dotnet nuget push "src/RabotCrypto.Nethermind.Db.Rocks/bin/Release/RabotCrypto.Nethermind.Db.Rocks.$VERSION.nupkg" --source "$NUGET_SOURCE"
 
-git tag "$VERSION"
-git push --tags
-
 echo "The packages has been published."
