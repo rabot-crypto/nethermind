fd870a1acafdd01d6b0ec3c384efabe68709532d
teneko <giteroneko@gmail.com>
2022-05-18 10:52:38 +0100

    Fixed missing binding assemblies in Nethermind.Db.Rocks
    Separated functional and binding packages by pre-release

--- rabot/scripts/publish-package.sh
+++ rabot/scripts/publish-package.sh
@@ -5,19 +5,29 @@
 set -e
 
 BINDING_DIR="binding"
-OFFICALS_DIR="officials"
+FUNCTIONAL_DIR="functional"
 ARTIFACTS_DIR="artifacts"
 
 CONFIGURATION=Release
+
+BINDING_TAG=$(dotnet vernuntii --config-path "$BINDING_DIR" --duplicate-version-fails)
+echo $BINDING_TAG
 
 # Pack projects for only binding purposes e.g. writing plugins
 dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Api"
 dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Db.Rocks"
 
+FUNCTIONAL_TAG=$(dotnet vernuntii --config-path "$FUNCTIONAL_DIR" --empty-caches --duplicate-version-fails)
+echo $FUNCTIONAL_TAG
+
 # Pack fully functional projects
-dotnet msbuild "$OFFICALS_DIR/RecursivePacker" -p:Configuration=$CONFIGURATION
+dotnet msbuild "$FUNCTIONAL_DIR/RecursivePacker" -p:Configuration=$CONFIGURATION
 
 # Push created packages
 find $ARTIFACTS_DIR -name "*.nupkg" -exec dotnet nuget push {} --source "$NUGET_SOURCE" \;
+echo "Packages have been published."
 
-echo "The packages have been published."
+git tag "$BINDING_TAG"
+git tag "$FUNCTIONAL_TAG"
+git push --tags
+echo "Git tags have been pushed"
