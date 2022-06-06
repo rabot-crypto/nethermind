90feab7f2de0e60b2e27e743f0c2c5afe629ee96
teneko <giteroneko@gmail.com>
2022-06-06 13:14:15 +0100

    replaced vernuntii by timestamp
    Added CopyToOutputDirectory and CopyToPublishDirectory to bindable DLL references

--- rabot/scripts/publish-package.sh
+++ rabot/scripts/publish-package.sh
@@ -8,20 +8,23 @@ BINDING_DIR="binding"
 FUNCTIONAL_DIR="functional"
 ARTIFACTS_DIR="artifacts"
 
+VERSION_CORE=1.13.1
+TIMESTAMP=$(git rev-list --count HEAD)
+
 CONFIGURATION=Release
 
-BINDING_TAG=$(dotnet vernuntii --config-path "$BINDING_DIR" --duplicate-version-fails)
+BINDING_TAG=$VERSION_CORE-binding.$TIMESTAMP
 echo $BINDING_TAG
 
 # Pack projects for only binding purposes e.g. writing plugins
-dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Api"
-dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Db.Rocks"
+dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Api" -p:PackageVersion="$BINDING_TAG"
+dotnet pack -c $CONFIGURATION "$BINDING_DIR/RabotCrypto.Nethermind.Db.Rocks" -p:PackageVersion="$BINDING_TAG"
 
-FUNCTIONAL_TAG=$(dotnet vernuntii --config-path "$FUNCTIONAL_DIR" --empty-caches --duplicate-version-fails)
+FUNCTIONAL_TAG=$VERSION_CORE-functional.$TIMESTAMP
 echo $FUNCTIONAL_TAG
 
 # Pack fully functional projects
-dotnet msbuild "$FUNCTIONAL_DIR/RecursivePacker" -p:Configuration=$CONFIGURATION
+dotnet msbuild "$FUNCTIONAL_DIR/RecursivePacker" -p:Configuration=$CONFIGURATION -p:PackageVersion="$FUNCTIONAL_TAG"
 
 # Push created packages
 find $ARTIFACTS_DIR -name "*.nupkg" -exec dotnet nuget push {} --source "$NUGET_SOURCE" \;
